2f74c66f1bea982bfaa31aa6e4cd0a44fde98ac3
Madhava2209 <52972972+Madhava2209@users.noreply.github.com>
2020-07-11 20:09:52 +0530

    done

---
+++ style.js
@@ -0,0 +1,31 @@
+function submitAnswers(){
+    var total=5;
+    var score=0;
+    var answers=["b","a","d","b","d"];
+
+    var q1=document.forms["quizform"]["q1"].value;
+    var q2=document.forms["quizform"]["q2"].value;
+    var q3=document.forms["quizform"]["q3"].value;
+    var q4=document.forms["quizform"]["q4"].value;
+    var q5=document.forms["quizform"]["q5"].value;
+
+
+    for (i=1; i<=total; i++){
+        if(eval("q"+i)==null || eval("q"+i)==''){
+            alert("You missed question "+i);
+            return false;
+        }
+    }
+    
+    for(i=1;i<=total;i++){
+        if(eval('q'+i)==answers[eval(i-1)]){
+            score++;
+        }
+    }
+
+    var results=document.getElementById("results");
+    results.innerHTML = '<h3> You scored <span>' +score+'</span> out of <span>' +total+'</span> </h3>';
+    alert("You scored "+score+" out of "+total);
+
+    return false;
+}
